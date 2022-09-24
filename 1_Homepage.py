87483e99639067f1ef92144145463bf60cb49599
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-24 19:06:08 -0300

    add joel greenblat image

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -30,7 +30,7 @@ if selected2 == "Value-invest":
     col1, col2, col3 = st.columns(3)
     with col1:
        st.header("A cat")
-       st.image("https://static.streamlit.io/examples/cat.jpg")
+       st.image("https://github.com/GustavoJannuzzi/Atiana/blob/638cbeffab869c22c7609f66ba2d99b8f208b364/imagens/1.png")
 
     with col2:
        st.header("A dog")
