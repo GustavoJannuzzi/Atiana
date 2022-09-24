23ad8df01ddfa437005d2b3b8d261bcc4413bdb4
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-24 16:36:05 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -6,17 +6,17 @@ import numpy as np
 import pandas as pd
 
 
-with st.sidebar:
-    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
-                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
+#with st.sidebar:
+#    choose = option_menu("Atiana", ["About", "Fundamentalistas", "Especulativas", "Estudos", "Contact"],
+#                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+#                         menu_icon="app-indicator", default_index=0,
+#                         styles={
+#        "container": {"padding": "5!important", "background-color": "#fafafa"},
+#        "icon": {"color": "black", "font-size": "25px"}, 
+#        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+#        "nav-link-selected": {"background-color": "#02ab21"},
+#    }
+#    )
 
 
 st.set_page_config(
