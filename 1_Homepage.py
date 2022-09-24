6f6afb77a3e34ecebba2c5f46bc62f8af8ae1a80
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-24 18:04:30 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -42,16 +42,16 @@ if selected2 == "Value-invest":
 # ESPECULATIVA - PAGE #
 if selected2 == "Epeculativa":
     with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+    with col2:
+       st.header("A dog")
+       st.image("https://static.streamlit.io/examples/dog.jpg")
 
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+    with col3:
+       st.header("An owl")
+       st.image("https://static.streamlit.io/examples/owl.jpg")
     
 # VALUE-INVEST - PAGE #
 if selected2 == "Estudos":
