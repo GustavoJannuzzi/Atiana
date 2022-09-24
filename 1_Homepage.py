977b90387c13f8e5dfe9a385b8dcd50b0bd87961
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-24 16:49:38 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title=("Estratégias Quantitativas")
 )
 
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+selected2 = option_menu(None, ["Home", "Fundamentalistas", "Epeculativas", 'Estudos'], 
+    icons=['house', 'cloud-upload', "list-task", 'book'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 selected2
