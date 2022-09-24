e34908f61e760a094285b3272a99bde9d104a61e
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-24 16:47:22 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -130,11 +130,11 @@ st.markdown("""
 
 
 # Menu Horizontal
-st.set_page_config(
-    page_title=("Estratégias Quantitativas")
-)
+#st.set_page_config(
+#    page_title=("Estratégias Quantitativas")
+#)
 
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-selected2
+#selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+#    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+#    menu_icon="cast", default_index=0, orientation="horizontal")
+#selected2
