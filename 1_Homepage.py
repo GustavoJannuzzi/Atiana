81f333f7afae10433f6644a9485574a19d165f37
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-25 10:39:59 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -27,7 +27,7 @@ if selected2 == "Home":
 
 # VALUE-INVEST - PAGE #
 if selected2 == "Value-invest":
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
        st.header("Joel Greenblatt - Magic Formula")
        st.image("imagens/joel_greenblat_mf.png")
@@ -35,10 +35,10 @@ if selected2 == "Value-invest":
     with col2:
        st.header("Quantamental - value investig")
        st.image("imagens/value_invest.png")
-
-    with col3:
-       st.header("corujinha")
-       st.image("https://static.streamlit.io/examples/owl.jpg")
+       st. markdown("A Magic Formula consiste em um procedimento de escolha de ações que consiste em selecionar ativos com bons fundamentos econômicos e que estão sendo negociados a um preço baixo.")
+       st.markdown("    1. Avalie o “rendimento de ganhos”. Isso significa que você precisa analisar se o preço da ação é um bom negócio. Isso pode ser feito calculando o lucro operacional e o preço da empresa para a qual a análise está sendo realizada;")
+       st.markdown"     2. Avalie o “retorno sobre o capital” ou ROIC. O retorno sobre o capital pode ser entendido como uma das métricas fundamentais que indicam a capacidade de uma empresa gerar lucros.")
+        
 
 # ESPECULATIVA - PAGE #
 if selected2 == "Epeculativa":
