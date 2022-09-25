01368f61e9f902413c2d23f4a8a131964e1e64a9
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-25 16:22:55 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -34,11 +34,11 @@ if selected2 == "Home":
     st.markdown("Através dessa aplicação eu gostaria de demonstrar meu interesse em aprender e contribuir com esta gigante fábrica de teses.")
     st.markdown("Aqui apresento alguns de meus projetos desenvolvidos sempre buscando criatividade alida a um breve conhecimento técnico.")
     st.button('Portfolio')
-    st.button(icon = 'clipboard2-data', on_click=style_button_row, kwargs={
-        'clicked_button_ix': 1, 'n_buttons': 4
-    })
-    
-    
+       if st.button('Portfolio'):
+           mf_url = "window.open('https://www.gustavojannuzziportfolio.com/')"
+           html = '<img src onerror="{}">'.format(mf_url)
+           div = Div(text=html)
+           st.bokeh_chart(div)
     
 # VALUE-INVEST - PAGE #
 if selected2 == "Value-invest":
