ee37a0b1d9d0849c16df85ceb96d943c0fcb6e94
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-09-25 15:54:04 -0300

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -34,7 +34,7 @@ if selected2 == "Home":
     st.markdown("Aqui apresento alguns de meus projetos desenvolvidos sempre buscando criatividade alida a um breve conhecimento técnico.")
     
     st.write("Streamlit Docs Example iframe")
-    components.iframe("https://docs.streamlit.io/en/latest")
+    components.iframe("https://www.gustavojannuzziportfolio.com/")
     st.write("different iframe test")
     components.iframe(src="http://smb-analytics-metabase.herokuapp.com/public/dashboard/afefddda-d5d4-43ed-83fd-307eab7ded3c", width=1285, height=1000, scrolling=True)
     
